771e7ac58b6607108c16ee00506c2f9bb85d3515
Dan Goodwin <dangoodwin1@gmail.com>
2025-11-21 14:45:15 +0000

    Support fullPage option

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ functions.http('screenshot', (req, res) => {
   if (req.method == "POST") {
     const captureId = crypto.randomUUID()
     const url = req.body.url
+    const fullPage = req.body.fullPage || false
 
     res.send({captureId: captureId, url: url});
 
@@ -14,7 +15,7 @@ functions.http('screenshot', (req, res) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
       await page.goto(url);
-      await page.screenshot({path: `${captureId}.png`});
+      await page.screenshot({path: `${captureId}.png`, fullPage: fullPage});
       await browser.close();
     })();
 
